fc8c6d31ae69db6b36d6e6ee65527b6a7c045083
maksimchup <93547008+maksimchup@users.noreply.github.com>
2021-11-02 01:18:02 +0300

    Update knn.py
    For points with 1 dimension. As far as I understand, no more is needed (based on the look of the "apply" function).

--- ex01/q5_knn_python/knn.py
+++ ex01/q5_knn_python/knn.py
@@ -11,4 +11,22 @@ def knn(samples, k):
 
     #####Insert your code here for subtask 5b#####
     # Compute the number of the samples created
+    N = len(samples)
+
+    pos = np.arange(-5.0, 5.0, 0.1)
+    den = np.zeros(len(pos))
+
+    # case of 1 dimension, 
+    for i in range(len(pos)):
+        x = pos[i]
+        # the distance to each point
+        dist = np.abs(x-samples)
+        # find indexes of k smallest values in dist
+        idx = np.argpartition(dist, k)
+        # volume is 2* dist to kth nearest point
+        V = 2*dist[idx[k-1]]
+
+        den[i] = k/(N*V)
+
+    estDensity = np.stack((pos, den), axis=1)    
     return estDensity
